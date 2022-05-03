58ac704cd8e4b4cc26004ff54ad85bc5da383f7a
Maik3000 <103969414+Maik3000@users.noreply.github.com>
2022-05-02 18:51:12 -0600

    Bubble sort opt added

--- algorithms/test.py
+++ algorithms/test.py
@@ -11,8 +11,17 @@ print("input: " , [2, 92, 8, -4, 0])
 print("output: " , bubble_sort([2, 92, 8, -4, 0]))
 '''
 
+'''
 from sorting.selection_sort import SelectionSort
 
 print("***Selection Sort***")
 print("input: " , [2, 92, 8, -4, 0])
 print("output: " , SelectionSort([2, 92, 8, -4, 0]))
+'''
+'''
+from sorting.bubble_sort_opt import bubble_sort_opt
+
+print("***Bubble Sort Optimizado***")
+print("input: " , [2, 92, 8, -4, 0])
+print("output: " , bubble_sort_opt([2, 92, 8, -4, 0]))
+'''
